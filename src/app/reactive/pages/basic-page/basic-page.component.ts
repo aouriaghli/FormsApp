3cdc0de9151c32d3226a91206f2e54438a9aa454
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-07-17 13:42:40 +0200

    Sec 17. Validaciones Parte1

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RXT 5090',
+  price: '250',
+  inStorage: '6',
+}
+
+@Component({
+  templateUrl: './basic-page.component.html',
+  styles: [
+  ]
+})
+export class BasicPageComponent implements OnInit {
+
+  // //Para crear un formulario reactivo
+  // public myForm: FormGroup = new FormGroup({
+  //   name : new FormControl(''),
+  //   price : new FormControl(0),
+  //   inStorage : new FormControl(0),
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  })
+
+  constructor( private fb : FormBuilder){ }
+
+  ngOnInit(): void {
+    // this.myForm.reset( rtx5090);
+  }
+
+  onSave():void{
+
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage:0});
+  }
+}
