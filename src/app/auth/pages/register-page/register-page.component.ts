419c838e2c25485325cd7ef633bf58ff731ef6c3
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-07-18 13:46:24 +0200

    Sec 18. Validadores sincronos

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
+//import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  public myForm : FormGroup = this.fb.group({
+    name : ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+    email: ['',[ Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+    username: ['',[ Validators.required, this.validatorsService.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  constructor(private fb : FormBuilder,
+              private validatorsService :ValidatorsService){}
+
+  isValidField(field : string){
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 }
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -17,15 +18,15 @@ export class DynamicPageComponent {
   });
 
   public newFavorite: FormControl = new FormControl('',[Validators.required]);
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder,
+    private validatorsService :ValidatorsService){}
 
   get favoriteGamesControls(){
     return this.myForm.get('favoriteGames')  as FormArray;
   }
 
-  isValidField( field: string ): boolean | null {
-    return this.myForm.controls[field].errors
-      && this.myForm.controls[field].touched;
+  isValidField(field : string){
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
   isValidFieldInArray( formArrary : FormArray, index: number){
