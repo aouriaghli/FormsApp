f694a3afab244ce75e35ee41cd8d120b83882f48
Ayoub Ouriaghli Romeo <ayoub.ouriaghli@soprasteria.com>
2023-07-21 13:52:23 +0200

    Sec18. Validador asyncrono email

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from 'src/app/shared/service/validators.service';
+import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
 //import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from 'src/app/shared/validators/validators';
 
 @Component({
@@ -12,14 +13,16 @@ export class RegisterPageComponent {
 
   public myForm : FormGroup = this.fb.group({
     name : ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
-    email: ['',[ Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+    //email: ['',[ Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [ new EmailValidatorService()]],
+    email: ['',[ Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [ this.emailValidator]],
     username: ['',[ Validators.required, this.validatorsService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   });
 
   constructor(private fb : FormBuilder,
-              private validatorsService :ValidatorsService){}
+              private validatorsService :ValidatorsService,
+              private emailValidator : EmailValidatorService){}
 
   isValidField(field : string){
     return this.validatorsService.isValidField(this.myForm, field);
---
+++ src/app/shared/validators/email-validator.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { Observable, delay, of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class EmailValidatorService implements AsyncValidator{
+
+
+  validate(control: AbstractControl ):  Observable<ValidationErrors | null> {
+    const email = control.value;
+
+    const httpCallObservable = new Observable<ValidationErrors|null>( (subscriber) => {
+
+      console.log({ email});
+
+      if (email === 'ayb.job@gmail.com'){
+        subscriber.next({ emailTalken : true});
+        subscriber.complete();
+        //return;
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+    }).pipe(
+      delay(3000)
+    );
+
+    return httpCallObservable;
+  }
+
+  // validate(control: AbstractControl ):  Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({ email })
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   );
+  // }
+
+  //Este es opcional
+  registerOnValidatorChange?(fn: () => void): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+
+}
